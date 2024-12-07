a9a928c86343baec56c82a71c279bb0d49bddf6f
Oscar Laird <olaird25@gmail.com>
2024-12-06 23:18:15 -0500

    vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
+import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -6,7 +7,10 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		// adapter: adapter()
+		adapter: vercel({
+			node: '20.x',
+		})
 	}
 };
 
